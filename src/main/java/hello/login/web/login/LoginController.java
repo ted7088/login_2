874398dc70636eeac0e1937809545d1e07bbf7cc
master <ted3131@diquest.com>
2024-12-28 14:14:59 +0900

    commit

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -55,7 +56,7 @@ public class LoginController {
 
         return "redirect:/";
     }
-    @PostMapping("/login")
+  //  @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult
             bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
@@ -76,6 +77,35 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+
+    /**
+     * 로그인 이후 redirect 처리
+     */
+    @PostMapping("/login")
+    public String loginV4(
+            @Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+            @RequestParam(defaultValue = "/") String redirectURL,
+            HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(),
+                form.getPassword());
+        log.info("login? {}", loginMember);
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        //redirectURL 적용
+        return "redirect:" + redirectURL;
+    }
+
 
     //@PostMapping("/logout")
     public String logout(HttpServletResponse response){
